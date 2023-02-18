d1826cbafa91d7155a26b3d0b48d56e9a57386f4
Renier Hingco <93526143+Shinigami823@users.noreply.github.com>
2023-02-18 20:14:52 +0800

    Update PrelimProj.java

--- PrelimProj.java
+++ PrelimProj.java
@@ -21,13 +21,13 @@ public class PrelimProj {
                 binaryInputValidation();
                 break;
             case "b":
-                
+                octalInputValidation();
                 break;
             case "c":
-                
+                decimalInputValidation();
                 break;
             case "d":
-                    
+                hexadecimalInputValidation();    
                 break;
             case "e":
                 System.out.println("Thank you for using this program!");
@@ -189,22 +189,251 @@ public class PrelimProj {
         System.out.print("Enter a binary number: ");
         String input = scanner.nextLine();
         boolean valid = true;
+
+        // Check that the input contains only 0s, 1s, and '.' characters
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
-            if (c != '0' && c != '1' && !(i == 0 && (c == '1' || c == '0')) && !(Character.isDigit(c) && (c == '0' || c == '1'))) {
+            if (c != '0' && c != '1' && c != '.') {
                 valid = false;
                 break;
             }
         }
+
+        // Check that the input has only one '.' character
+        int countDot = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '.') {
+                countDot++;
+            }
+        }
+        if (countDot > 1) {
+            valid = false;
+        }
+
         if (valid) {
-            System.out.println("Valid binary number: " + input);
+            // Split the input into its integer and fractional parts
+            String[] parts = input.split("\\.");
+            String integerPart = parts[0];
+            String fractionalPart = parts.length > 1 ? parts[1] : "";
+
+            // Check that the integer part contains only 0s and 1s
+            for (int i = 0; i < integerPart.length(); i++) {
+                char c = integerPart.charAt(i);
+                if (c != '0' && c != '1') {
+                    valid = false;
+                    break;
+                }
+            }
+
+            // Check that the fractional part contains only 0s and 1s
+            for (int i = 0; i < fractionalPart.length(); i++) {
+                char c = fractionalPart.charAt(i);
+                if (c != '0' && c != '1') {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        if (valid) {
+            double decimal = binaryToDecimal(input);
+            String octal = decimalToOctal(decimal);
+            String hexadecimal = decimalToHexadecimal(decimal);
+            print(input, octal, decimal + "", hexadecimal);
         } else {
             System.out.println("Invalid binary number. Please enter a binary number that contains only 0s, 1s, and decimal digits that are 0 or 1.");
             binaryInputValidation();
         }
     }
 
+    public static void octalInputValidation() {
+        System.out.print("Enter an octal number: ");
+        String input = scanner.nextLine();
+        boolean valid = true;
 
+        // Check that the input contains only 0 to 7 digits and '.' characters
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c != '.' && (c < '0' || c > '7')) {
+                valid = false;
+                break;
+            }
+        }
+
+        // Check that the input has only one '.' character
+        int countDot = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '.') {
+                countDot++;
+            }
+        }
+        if (countDot > 1) {
+            valid = false;
+        }
+
+        if (valid) {
+            // Split the input into its integer and fractional parts
+            String[] parts = input.split("\\.");
+            String integerPart = parts[0];
+            String fractionalPart = parts.length > 1 ? parts[1] : "";
+
+            // Check that the integer part contains only 0 to 7 digits
+            for (int i = 0; i < integerPart.length(); i++) {
+                char c = integerPart.charAt(i);
+                if (c < '0' || c > '7') {
+                    valid = false;
+                    break;
+                }
+            }
+
+            // Check that the fractional part contains only 0 to 7 digits
+            for (int i = 0; i < fractionalPart.length(); i++) {
+                char c = fractionalPart.charAt(i);
+                if (c < '0' || c > '7') {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        if (valid) {
+            double decimal = octalToDecimal(input);
+            String binary = decimalToBinary(decimal);
+            String hexadecimal = decimalToHexadecimal(decimal);
+            print(binary, input, decimal + "", hexadecimal);
+        } else {
+            System.out.println("Invalid octal number. Please enter an octal number that contains only digits 0 to 7, and a decimal point followed by digits 0 to 7 for the fractional part.");
+            octalInputValidation();
+        }
+    }
+
+    public static void decimalInputValidation() {
+        System.out.print("Enter a decimal number: ");
+        String input = scanner.nextLine();
+        boolean valid = true;
+
+        // Check that the input contains only digits and '.' characters
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c != '.' && (c < '0' || c > '9')) {
+                valid = false;
+                break;
+            }
+        }
+
+        // Check that the input has only one '.' character
+        int countDot = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '.') {
+                countDot++;
+            }
+        }
+        if (countDot > 1) {
+            valid = false;
+        }
+
+        if (valid) {
+            // Split the input into its integer and fractional parts
+            String[] parts = input.split("\\.");
+            String integerPart = parts[0];
+            String fractionalPart = parts.length > 1 ? parts[1] : "";
+
+            // Check that the integer part contains only digits
+            for (int i = 0; i < integerPart.length(); i++) {
+                char c = integerPart.charAt(i);
+                if (c < '0' || c > '9') {
+                    valid = false;
+                    break;
+                }
+            }
+
+            // Check that the fractional part contains only digits
+            for (int i = 0; i < fractionalPart.length(); i++) {
+                char c = fractionalPart.charAt(i);
+                if (c < '0' || c > '9') {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        if (valid) {
+            double decimal = Double.parseDouble(input);
+            String binary = decimalToBinary(decimal);
+            String octal = decimalToOctal(decimal);
+            String hexadecimal = decimalToHexadecimal(decimal);
+            print(binary, octal, decimal + "", hexadecimal);
+        } else {
+            System.out.println("Invalid decimal number. Please enter a decimal number that contains only digits 0 to 9, and a decimal point followed by digits 0 to 9 for the fractional part.");
+            decimalInputValidation();
+        }
+    }
+
+    public static void hexadecimalInputValidation() {
+        System.out.print("Enter a hexadecimal number: ");
+        String input = scanner.nextLine();
+        boolean valid = true;
+
+        // Check that the input contains only hex characters and '.' characters
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c != '.' && !((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))) {
+                valid = false;
+                break;
+            }
+        }
+
+        // Check that the input has only one '.' character
+        int countDot = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c == '.') {
+                countDot++;
+            }
+        }
+        if (countDot > 1) {
+            valid = false;
+        }
+
+        if (valid) {
+            // Split the input into its integer and fractional parts
+            String[] parts = input.split("\\.");
+            String integerPart = parts[0];
+            String fractionalPart = parts.length > 1 ? parts[1] : "";
+
+            // Check that the integer part contains only hex characters
+            for (int i = 0; i < integerPart.length(); i++) {
+                char c = integerPart.charAt(i);
+                if (!((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            // Check that the fractional part contains only hex characters
+            for (int i = 0; i < fractionalPart.length(); i++) {
+                char c = fractionalPart.charAt(i);
+                if (!((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))) {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+
+        if (valid) {
+            double decimal = hexadecimalToDecimal(input);
+            String binary = decimalToBinary(decimal);
+            String octal = decimalToOctal(decimal);
+            print(binary, octal, decimal + "", input);
+        } else {
+            System.out.println("Invalid hexadecimal number. Please enter a hexadecimal number that contains only digits 0 to 9 and letters A to F, and a decimal point followed by digits 0 to 9 and letters A to F for the fractional part.");
+            hexadecimalInputValidation();
+        }
+    }
+    
     public static void print(String binary, String octal, String decimal, String hexadecimal) {
         System.out.println("Binary: " + binary);
         System.out.println("Octal: " +  octal);
